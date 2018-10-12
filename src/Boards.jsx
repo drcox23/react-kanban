77948d7c31fdf7dc03f60b93bf323703b99a1764
doug <drcox23@gmail.com>
2018-10-11 22:16:58 -1000

    reconfigure the components, need to work on filter still

--- src/Boards.jsx
+++ src/Boards.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-import Tasks from './components/Tasks.jsx'
+import Tasks from './components/Tasks.jsx';
+import axios from "axios"
 
 
 class Boards extends Component {
@@ -20,17 +21,51 @@ class Boards extends Component {
           id: 3,
           title: "Done"
         }
-      ]
+      ],
+      tasks: []
     };
   }
 
+  componentDidMount() {
+    axios
+      .get("/tasks")
+      .then(tasks => {
+        // console.log("tasks", tasks);
+        this.setState({ tasks: tasks.data });
+      })
+      .catch(err => {
+        console.log("errors", err);
+      });
+  }
+
   render() {
-    // console.log("what is the state?: ", this.state);
+    console.log("what is the state?: ", this.state);
+    const daBoard = this.state.boardsName.title
+    console.log("CHECK CHECK", daBoard)
     return (
       <div className="Boards">
-        {this.state.boardsName.map( item => <div className="Categories" key={item.id}>{item.title}
-        <Tasks />
-        </div>)}
+        {this.state.boardsName.map( item => {
+          // console.log(item)
+          return (<div className="Categories" key={item.id}>{item.title}
+
+          <div> 
+            {/* if (item.title === "") {
+              
+            } */}
+          </div>
+
+          {/* {this.state.boardsName.map(item => {
+            console.log("DAAA", item)
+            return <CardPick status={this.state.boardsName}/>
+          })} */}
+          
+          {/* {this.state.tasks.map( tasks => {
+            console.log("HUHHHHHH", tasks)
+            return <DoneCards tasks={this.state.tasks}/>
+          })}
+          <CardPick status={this.state.boardsName}/> */}
+          </div>)
+        })}
 
         
         {/* <BoardTitles items={this.state.items}/> */}
@@ -39,10 +74,82 @@ class Boards extends Component {
   }
 }
 
-  // function BoardTitles(props){
-  //   console.log("how does this work?????: ", props)
-  //   return props.boardsName.map( item => <div className={item.title}> <Boards key={item.id} title={item.title}/></div>)
-  // }
+function CardPick(props) {
+  console.log("WHATTT", props)
+  const status = props.status.title;
+  if(status === "Done"){
+    return <DoneCards />
+  } else if (status === "In Queue") {
+    return <QueueCards />
+  } else if (status === "In Progress"){
+    return <ProgressCards />
+  }
+}
+
+
+// filter for cards with a In Progress status
+function ProgressCards(props) {
+  const filteredArr = props.tasks.filter(tasks => {
+    return tasks.status === "In Progress";
+  });
+  console.log("In Progress FILTER", filteredArr);
+  return filteredArr.map(tasks => {
+    return (
+      <Tasks Cards
+        key={tasks.id}
+        title={tasks.title}
+        priority={tasks.priority}
+        status={tasks.status}
+        assignedTo={tasks.assignedTo}
+      />
+    );
+  });
+}
+
+// filter for cards with a In Queue status
+function QueueCards(props) {
+  // console.log("CAAARRRDS", props);
+  const filteredArr = props.tasks.filter(tasks => {
+    return tasks.status === "In Queue";
+  });
+  console.log("In Queue FILTER", filteredArr);
+  return filteredArr.map(tasks => {
+    return (
+      <Tasks Cards
+        key={tasks.id}
+        title={tasks.title}
+        priority={tasks.priority}
+        status={tasks.status}
+        assignedTo={tasks.assignedTo}
+      />
+    );
+  });
+}
+
+// filter for cards with a Done status
+function DoneCards(props) {
+  // console.log("PRRRROOOOPPP", props)
+  const filteredArr = props.tasks.filter(tasks => {
+    return tasks.status === "Done";
+  });
+  console.log("DONE FILTER", filteredArr);
+  return filteredArr.map(tasks => {
+    // console.log("TAAASSSSKKKS", tasks)
+    return (
+      <Tasks 
+        key={tasks.id}
+        title={tasks.title}
+        priority={tasks.priority}
+        status={tasks.status}
+        assignedTo={tasks.assignedTo}
+      />
+    );
+  });
+}
+
+
+
+
 
 
 export default Boards;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "../App.css";
-import {
-  getItemsFromFakeXHR,
-  addItemToFakeXHR,
-  deleteItemByIdFromFakeXHR
-} from "../db/data.js";
-import axios from "axios";
+// import {
+//   getItemsFromFakeXHR,
+//   addItemToFakeXHR,
+//   deleteItemByIdFromFakeXHR
+// } from "../db/data.js";
+// import axios from "axios";
 
 // function TaskList(props){
 //   console.log("what is props??????: ", props)
@@ -15,36 +15,32 @@ import axios from "axios";
 class Tasks extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      tasks: []
-    };
   }
 
-  componentDidMount() {
-    axios
-      .get("/tasks")
-      .then(tasks => {
-        // console.log("tasks", tasks);
-        this.setState({ tasks: tasks.data });
-      })
-      .catch(err => {
-        console.log("errors", err);
-      });
-    // getItemsFromFakeXHR()
-    //   .then( tasks => {
-    //     // console.log("what am i getting??? ", items)
-    //     this.setState({ tasks })
-    //   }, function() {
-    //     console.log('this.state updated', this.state)
-    //   })
-  }
+  // componentDidMount() {
+  //   axios
+  //     .get("/tasks")
+  //     .then(tasks => {
+  //       // console.log("tasks", tasks);
+  //       this.setState({ tasks: tasks.data });
+  //     })
+  //     .catch(err => {
+  //       console.log("errors", err);
+  //     });
+  // }
 
   render() {
-    // console.log("what can i see??? ", this.state);
+    // console.log("what can i see??? ", this.props);
     return (
       <div>
-        <CardList tasks={this.state.tasks} />
-        {/* <QueueCards tasks={this.state.tasks}/> */}
+        <Cards key={this.props.id}
+        title={this.props.title}
+        priority={this.props.priority}
+        status={this.props.status}
+        assignedTo={this.props.assignedTo}/>
+        {/* <QueueCards tasks={this.state.tasks} />
+        <ProgressCards tasks={this.state.tasks} />
+        <DoneCards tasks={this.state.tasks} /> */}
       </div>
 
       // <div className={this.state.status}>
@@ -54,34 +50,76 @@ class Tasks extends Component {
   }
 }
 
-function CardList(props) {
-  console.log("what is this???", props);
-  return props.tasks.map(tasks => (
-    <Cards
-      key={tasks.id}
-      title={tasks.title}
-      priority={tasks.priority}
-      status={tasks.status}
-      assignedTo={tasks.assignedTo}
-    />
-  ));
-}
+// function CardList(props) {
+//   console.log("what is this???", props);
+//   return props.tasks.map(tasks => (
+//     <Cards
+//       key={tasks.id}
+//       title={tasks.title}
+//       priority={tasks.priority}
+//       status={tasks.status}
+//       assignedTo={tasks.assignedTo}
+//     />
+//   ));
+// }
 
-function QueueCards(props) {
-  console.log("CAAARRRDS", props);
-  return props.filter((status) => {
-      console.log("whats going on???", status)
-      return props.status === "Done";
-    })
-    .map(status => {
-      // console.log("HUHHHH", tasks);
-      return (
-        <div className={status.status}>
-          <Cards title={status.status} />
-        </div>
-      );
-    });
-}
+// // filter for cards with a Done status
+// function DoneCards(props) {
+//   const filteredArr = props.tasks.filter(tasks => {
+//     return tasks.status === "Done";
+//   });
+//   console.log("DONE FILTER", filteredArr);
+//   return filteredArr.map(tasks => {
+//     return (
+//       <Cards
+//         key={tasks.id}
+//         title={tasks.title}
+//         priority={tasks.priority}
+//         status={tasks.status}
+//         assignedTo={tasks.assignedTo}
+//       />
+//     );
+//   });
+// }
+
+// // filter for cards with a In Progress status
+// function ProgressCards(props) {
+//   const filteredArr = props.tasks.filter(tasks => {
+//     return tasks.status === "In Progress";
+//   });
+//   console.log("In Progress FILTER", filteredArr);
+//   return filteredArr.map(tasks => {
+//     return (
+//       <Cards
+//         key={tasks.id}
+//         title={tasks.title}
+//         priority={tasks.priority}
+//         status={tasks.status}
+//         assignedTo={tasks.assignedTo}
+//       />
+//     );
+//   });
+// }
+
+// // filter for cards with a In Queue status
+// function QueueCards(props) {
+//   // console.log("CAAARRRDS", props);
+//   const filteredArr = props.tasks.filter(tasks => {
+//     return tasks.status === "In Queue";
+//   });
+//   console.log("In Queue FILTER", filteredArr);
+//   return filteredArr.map(tasks => {
+//     return (
+//       <Cards
+//         key={tasks.id}
+//         title={tasks.title}
+//         priority={tasks.priority}
+//         status={tasks.status}
+//         assignedTo={tasks.assignedTo}
+//       />
+//     );
+//   });
+// }
 
 function Cards(props) {
   // console.log("Check me out!!!", props)
